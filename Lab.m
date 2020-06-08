c94b73d6e2e19d723a20d26a8e1bebc44e26aeaf
YY-FANG <58865983+YY-FANG@users.noreply.github.com>
2020-06-08 10:21:35 +0800

    Add files via upload

--- Lab.m
+++ Lab.m
@@ -9,13 +9,13 @@ B_up = [0;0;35.06604733;34.99714604];
 
 %% Design
 % desired closed loop eigenvalues
-P = [-4+1i -4-1i -20 -21];
+P = [-6 -8 -10 -12];
 
 % downward position K
-K_down = acker(A_down, B_down, P);
+K_down = acker(A_down, B_down, P)
 
 % upward position K
-K_up = acker(A_up, B_up, P);
+K_up = acker(A_up, B_up, P)
 
 % LQR
 Q_down = eye(4);
@@ -28,6 +28,6 @@ Q_up(1,1) = 0.01;
 R_up = 100;
 K_up_LQR = lqr(A_up, B_up, Q_up, R_up);
 
-Poles_down = eig(A_down-B_down*K_down_LQR);
-Poles_up = eig(A_up-B_up*K_up_LQR);
+Poles_down = eig(A_down-B_down*K_down)
+Poles_up = eig(A_up-B_up*K_up)
 
